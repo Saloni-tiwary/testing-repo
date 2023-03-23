7e70d853a56222e26bc530f5f48803789d3f86df
saloni-tiiwary <saloni.tiwary@anywhere.co>
2023-03-23 12:42:33 +0530

    added 2 workers

--- playwright.config.js
+++ playwright.config.js
@@ -38,9 +38,9 @@ const { devices,expect} = require('@playwright/test');
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 0 : 0,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 2 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
    
