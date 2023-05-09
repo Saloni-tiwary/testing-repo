ac2068f9f7715d5c88463f35df826886d58f7523
saloni-tiiwary <saloni.tiwary@anywhere.co>
2023-05-09 10:26:06 +0530

    added all files in yml

--- pageobject_pagefile/recommending-sample-link-noclick.js
+++ pageobject_pagefile/recommending-sample-link-noclick.js
@@ -1,5 +1,8 @@
 const { expect } = require("@playwright/test");
-const{LinkBuilder} = require('./linkdetails')
+const{LinkBuilder} = require('./linkdetails');
+
+const authorizedFetch=require ('../Config/global-setup2')
+
 class RecommendSampleLinkWithOutClick{
 
 constructor(page){
@@ -15,7 +18,7 @@ this.CloseHeader= page.locator("i#close-icon");
 }
 async recommendingSampleLinkWithoutClick(Url,Min,linkname){
  
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6ImQxNzJhY2MxMDJjMGRhYWQzNThiZmM5ZDYwZWEyMWJhOWVjY2I2ZWUifQ.eyJpc3MiOiJodHRwczovL2FueXdoZXJlLnN0YWdpbmcuYW55d2hlcmVhdXRoLmNvbSIsImlhdCI6MTY4MTIxODIyOSwicHJval9pZCI6ImFueXdoZXJld29ya3MiLCJ0eXBlIjoidXNlciIsInN1YiI6IjBjZWY5MDExLTM4YTItNDVjOC1hNjdmLTRmNjQ2OWM0MDlmYyIsImV4cCI6MTY4MTIyNTQyOSwianRpIjoiZDE1NzY0Y0RRVkFNNTBpWiJ9.dtbIJyECneV5vDIUXDySzI4vZcgpWLVuNduoI7H6y9wlTEqqEcG7pKllEztnNl5pbWkhgF3uMJ46JtVvU9wHPLwfRfnLJD2kGD67XqFJjcX6KlwYI3rOZeDgNjEb4SiAVp7MYuLwXRx53O-yAHxXVHt4Y5fZEyUdPkomZDU5ETFxJlNM5_7RWBaBq7val8COPjqwYUzdyGSKmAkOa0iwmdb5O3ApxNrrixE5YKb4mhO8_uiWObNgvlY8rrwRwm0Z-uQ1licJL6s1LDQw-Hi7WnFzGYUp2COLoaPP9WDNpFyHtGK0spPBAETt5s-AO34rPEip6wmObe-vDNCGrSBxwg"
+ 
 
     const link = new LinkBuilder()
   .setName(linkname)
@@ -45,11 +48,11 @@ const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6ImQxNzJhY2MxMDJjM
   .setIsAllowDuplicate(false)
   .setSourceType("university")
   .build();
-  const response = await fetch('https://stag.adaptiveu.info/api/v1/university/anywhereworks/link', {
+  const response = await authorizedFetch('https://stag.adaptiveu.info/api/v1/university/anywhereworks/link', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}` // Use the access token obtained during authentication
+        //Authorization: `Bearer ${accessToken}` // Use the access token obtained during authentication
       },
       body: JSON.stringify(link)
         
--- tests/access.js
+++
@@ -1,3 +0,0 @@
-const mySecret = process.env.TEST_SECRET;
-console.log(`The value of MY_SECRET is ${mySecret}`);
-//console.log(process.env.TEST_SECRET)
