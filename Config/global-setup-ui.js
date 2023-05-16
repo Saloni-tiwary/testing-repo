4b62eac7844db8ff231c886a6eeaeaa12f3870d2
Saloni_tiwary <113579966+Saloni-Tiiwary@users.noreply.github.com>
2023-05-16 14:33:35 +0530

    Update global-setup-ui.js

--- Config/global-setup-ui.js
+++ Config/global-setup-ui.js
@@ -1,12 +1,13 @@
-const { chromium,expect } = require('@playwright/test');
-
+import { chromium,expect } from '@playwright/test';
+import { config } from 'dotenv';
+ config()
  
 module.exports=async config => {
   const browser = await chromium.launch({headless: true});
   const page = await browser.newPage();
   await page.goto('https://my.staging.adaptiveu.app/');
-  await page.locator("#email").type("shakti.choudhary@anywhere.co");
-  await page.locator( "[type='password']").type("password");
+  await page.locator("#email").type("process.env.USERNAME");
+  await page.locator( "[type='password']").type("process.enc.PASSWORD");
   await page.locator("#login_submit").click();
   await expect(page.locator("#sidebar-app-adaptiveu")).toBeVisible({timeout:50000});
   //await page.locator('#close-icon svg').click();
