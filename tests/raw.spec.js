9a96cc707b3cda54b21c5aacd1858980bae8dccd
Saloni-tiwary <121880445+Saloni-tiwary@users.noreply.github.com>
2023-06-09 17:47:56 +0530

    Update raw.spec.js

--- tests/raw.spec.js
+++ tests/raw.spec.js
@@ -1,15 +1,12 @@
 const { test, expect, request } = require("@playwright/test");
 const { Agent } = require("http");
 
-test("verify that link outer card more options are selectable", async ({
-  page})=>{
-
-   await page.goto("https://my.staging.adaptiveu.app/");
-   await page.locator("#email").type("shakti.choudhary@anywhere.co");
-  await page.locator("[type='password']").type("password");
-   await page.locator("#login_submit").click();
-   await expect(page.locator("[data-awd-tooltip='Learning']")).toHaveClass(
-     "awd-tooltip awd-tooltip--right awd-tooltip-sm"
-   );
-//   await page.locator("#close-icon svg").click();
- });
+test("verify when more settings is clicked the edit popup expands unvieling more options", async ({
+  page,
+}) => {
+  await page.locator(".challenge-image").nth(0).hover();
+  await page.locator(".link-dropdown").nth(0).click();
+  await page.locator("[data-status='edit']").nth(0).click();
+  await page.locator(".more-settings").click();
+  await expect(page.locator(".populate")).toBeVisible();
+});
