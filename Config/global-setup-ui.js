30d58a0b8156356dbd9aecefa1ed7453f834efa7
Saloni-tiwary <121880445+Saloni-tiwary@users.noreply.github.com>
2023-06-09 17:19:42 +0530

    Update global-setup-ui.js

--- Config/global-setup-ui.js
+++ Config/global-setup-ui.js
@@ -6,8 +6,8 @@ module.exports=async config => {
   const browser = await chromium.launch({headless: true});
   const page = await browser.newPage();
   await page.goto('https://my.staging.adaptiveu.app/');
-  await page.locator("#email").type("process.env.USERNAME");
-  await page.locator( "[type='password']").type("process.enc.PASSWORD");
+  await page.locator("#email").type("shakti.choudhary@anywhere.co");
+  await page.locator( "[type='password']").type("password");
   await page.locator("#login_submit").click();
   await expect(page.locator("#sidebar-app-adaptiveu")).toBeVisible({timeout:50000});
   //await page.locator('#close-icon svg').click();
