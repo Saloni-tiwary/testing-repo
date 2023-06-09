b6d41204ee22d1bd34f2f73c385473c6e3614f8b
Saloni-tiwary <121880445+Saloni-tiwary@users.noreply.github.com>
2023-06-09 17:39:43 +0530

    Update raw.spec.js

--- tests/raw.spec.js
+++ tests/raw.spec.js
@@ -3,36 +3,13 @@ const { Agent } = require("http");
 
 test("verify that link outer card more options are selectable", async ({
   page,
-}) => {
-  await page.locator(".challenge-image").nth(0).hover();
-  await page.locator(".link-dropdown").nth(0).click();
-  await expect(page.locator(".edit-content").nth(0)).toContainText("Edit");
-});
 
-
-test("verify that link outer card add to learn options are clickable", async ({
-  page,
-}) => {
-  await page.locator('[data-tippy-content="Add to Learn" ]').nth(0).click();
-  await expect(
-    page.locator("[data-tippy-content='Added to Learn' ]").nth(0)
-  ).toBeEnabled();
-});
-
-
-test("verify on clicking menu, sidebar disappers if visible", async ({
-  page,
-}) => {
-  await page.locator(".menu-icon").click();
-  await expect(page.locator(".au")).toHaveClass("au");
-});
-
-
-test("verify on clicking the menu sidebar appears, if not visible before", async ({
-  page,
-}) => {
-  await page.locator(".menu-icon").click();
-  await page.locator(".menu-icon").click();
-  await expect(page.locator(".sidebar-open")).toHaveClass("au sidebar-open");
-});
-
+   await page.goto("https://my.staging.adaptiveu.app/");
+   await page.locator("#email").type("shakti.choudhary@anywhere.co");
+  await page.locator("[type='password']").type("password");
+   await page.locator("#login_submit").click();
+   await expect(page.locator("[data-awd-tooltip='Learning']")).toHaveClass(
+     "awd-tooltip awd-tooltip--right awd-tooltip-sm"
+   );
+//   await page.locator("#close-icon svg").click();
+ });
