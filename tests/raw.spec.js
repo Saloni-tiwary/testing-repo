a70aa237a06a5f4a5f765a5f347f5d140f2da5e4
Saloni-tiwary <121880445+Saloni-tiwary@users.noreply.github.com>
2023-06-09 17:53:42 +0530

    Update raw.spec.js

--- tests/raw.spec.js
+++ tests/raw.spec.js
@@ -4,6 +4,7 @@ const { Agent } = require("http");
 test("verify when more settings is clicked the edit popup expands unvieling more options", async ({
   page,
 }) => {
+  await page.goto("https://my.staging.adaptiveu.app/");
   await page.locator(".challenge-image").nth(0).hover();
   await page.locator(".link-dropdown").nth(0).click();
   await page.locator("[data-status='edit']").nth(0).click();
